c2c8d0725ac4c25536ba90703cd348e476b101a4
MahendraSH <maahi2001sh@gmail.com>
2024-01-29 16:54:32 +0530

    db connect

---
+++ node-js-task-mg/config/DBconnect.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const dbConnect = () => {
+    mongoose.connect(process.env.MONGODB_URL,)
+        .then((data) => {
+            console.log("db connection  succesful ");
+            console.log(`Mongodb connected with server: ${data.connection.host}`);
+            console.log(`db connected : ${data.connection.name}`);
+        }).catch((err) => {
+            console.log(err);
+        });
+}
+
+module.exports = dbConnect;
--- node-js-task-mg/index.js
+++ node-js-task-mg/index.js
@@ -1,10 +1,11 @@
-const app = require("./app");
-const dotenv = require("dotenv").config({});
+
+const app = require("./app")
+const dbConnect = require("./config/DBconnect")
+const dotenv = require("dotenv").config({ path: "./.env" })
 const PORT = process.env.PORT || 4000;
 
-
+dbConnect();
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     console.log(`http://localhost:${PORT}`);
 });
-
